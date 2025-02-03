82bf973eb00d2cfdc53b489d35d931c7cce93a3d
ryangithub43 <rmoll001@mymail.lausd.net>
2025-02-03 10:59:58 -0800

    Update myStory.java
    asdfsadf

--- myStory.java
+++ myStory.java
@@ -3,7 +3,7 @@ import org.code.media.*;
 
 public class myStory extends Scene {
   
-  private static final String[][] foodList = { {"Fries, Fried Chicken"},
+  private static final String[][] foodList = { {"Fries", "Fried Chicken"},
                                               {"Macaroni and Cheese", "Spaghetti"} }; 
 
   private static final String[][] imageList = { {"fries.jpg", "fried-chicken.jpg"},
@@ -30,6 +30,35 @@ public class myStory extends Scene {
     drawImage(image2, 50, 300, 100); 
 
     pause(1.0);
+
+    Filter image3 = new Filter(imageList[0][1]); 
+
+    drawImage(image3, 250, 300, 100); 
+    pause(0.5);
+    image3.threshold(30);
+    pause(1.0);
+
+    clear("white");
+
+    drawText(foodList[1][0], 50, 100);
+    pause(0.5);
+    drawText(foodList[1][1], 250, 100);
+    pause(0.5);
+
+    Filter image4 = new Filter(imageList[1][0]);
+
+    drawImage(image4, 50, 300, 100);
+    pause(0.5);
+    image4.thresholdBlueAndGreen(30);
+    drawImage(image4, 50, 300, 100); 
+    
+    Filter image5 = new Filter(imageList[1][1]); 
+
+    drawImage(image5, 250, 300, 100); 
+    pause(0.5);
+    image5.makeSlightlyNegative();
+    pause(1.0);
+
     
   }
 }
